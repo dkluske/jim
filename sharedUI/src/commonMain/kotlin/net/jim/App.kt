baf8bd0d25ba2adb8d9860c04d9452aac6bb04e5
dkluske <dome.k28@yahoo.com>
2026-02-14 22:21:23 +0100

    add exercise updating mechanism and database initialization to app

--- sharedUI/src/commonMain/kotlin/net/jim/App.kt
+++ sharedUI/src/commonMain/kotlin/net/jim/App.kt
@@ -2,7 +2,13 @@ package net.jim
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import jim.sharedui.generated.resources.Res
+import kotlinx.coroutines.launch
 import net.jim.data.DatabaseDriverManager
+import net.jim.data.JimDatabaseManager
+import net.jim.data.models.JsonExercise
+import net.jim.data.table.JsonExerciseTable
 import net.jim.theme.AppTheme
 import net.jim.views.MainView
 import net.jim.views.MainViewModel
@@ -12,6 +18,20 @@ import net.jim.views.Root
 fun App(
     databaseDriverManager: DatabaseDriverManager
 ) = AppTheme {
+    val database = JimDatabaseManager.createDatabase(databaseDriverManager)
+    JimDatabaseManager.initTables(database)
+
+    val appCoroutineScope = rememberCoroutineScope()
+
+    appCoroutineScope.launch {
+        ((JsonExerciseTable.getMaxStoredRevision() + 1)..JsonExerciseTable.MAX_REVISION).flatMap {
+            val bytes = Res.readBytes("files/exercises/exercises_$it.json")
+            JimDatabaseManager.json.decodeFromString<List<JsonExercise>>(bytes.decodeToString())
+        }.forEach { exercise ->
+            JsonExerciseTable.save(exercise)
+        }
+    }
+
     val root = remember { Root() }
     MainView(
         vm = MainViewModel(
