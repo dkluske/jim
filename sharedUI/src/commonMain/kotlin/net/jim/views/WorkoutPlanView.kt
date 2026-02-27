3f0899c839ae7d91e89f2296662591725fc901c6
dkluske <dome.k28@yahoo.com>
2026-02-27 08:31:10 +0100

    add frame for WorkoutPlanView.kt (styling missing)

--- sharedUI/src/commonMain/kotlin/net/jim/views/WorkoutPlanView.kt
+++ sharedUI/src/commonMain/kotlin/net/jim/views/WorkoutPlanView.kt
@@ -4,18 +4,19 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import jim.sharedui.generated.resources.Res
+import jim.sharedui.generated.resources.addWorkoutPlanPart
 import jim.sharedui.generated.resources.newWorkoutPlan
+import jim.sharedui.generated.resources.save
 import net.jim.components.utils.JimCard
 import net.jim.data.models.WorkoutPlan
 import net.jim.data.models.WorkoutPlanPart
@@ -35,6 +36,13 @@ data class WorkoutPlanViewModel(
     fun loadWorkoutPlanParts(workoutPlanId: Uuid): List<WorkoutPlanPart> {
         return WorkoutPlanPartTable.getByWorkoutPlanId(workoutPlanId)
     }
+
+    fun saveWorkoutPlan(workoutPlan: WorkoutPlan, parts: List<WorkoutPlanPart>) {
+        WorkoutPlanTable.save(workoutPlan)
+        parts.forEach {
+            WorkoutPlanPartTable.save(it)
+        }
+    }
 }
 
 @Composable
@@ -48,6 +56,8 @@ fun WorkoutPlanView(
     LaunchedEffect(Unit) {
         vm.workoutId?.let {
             workout = vm.loadWorkout(it)
+            planParts.clear()
+            planParts.addAll(vm.loadWorkoutPlanParts(it))
         }
     }
 
@@ -65,6 +75,13 @@ fun WorkoutPlanView(
             horizontalArrangement = Arrangement.Start,
             verticalAlignment = Alignment.CenterVertically,
         ) {
+            IconButton(
+                onClick = { /* navigate back */ }
+            ) {
+                Text(
+                    text = "<"
+                )
+            }
             if (editWorkoutName) {
                 TextField(
                     value = workout?.name ?: stringResource(Res.string.newWorkoutPlan),
@@ -109,8 +126,77 @@ fun WorkoutPlanView(
                     state = lazyListState
                 ) {
                     items(planParts.size) { index ->
-
+                        ListItem(
+                            headlineContent = {
+                                // Plan name
+                                Text(
+                                    text = planParts[index].name
+                                )
+                            },
+                            trailingContent = {
+                                // Quantity Spinner
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                ) {
+                                    if (planParts[index].quantity == 1) {
+                                        IconButton(
+                                            onClick = { planParts.removeAt(index) }
+                                        ) {
+                                            Icon(Icons.Filled.Delete, "delete part")
+                                        }
+                                    } else {
+                                        IconButton(
+                                            onClick = {
+                                                planParts[index] =
+                                                    planParts[index].copy(quantity = planParts[index].quantity - 1)
+                                            }
+                                        ) {
+                                            Text(
+                                                text = "-"
+                                            )
+                                        }
+                                    }
+                                    Text(
+                                        text = planParts[index].quantity.toString(),
+                                    )
+                                    IconButton(
+                                        onClick = {
+                                            planParts[index] =
+                                                planParts[index].copy(quantity = planParts[index].quantity + 1)
+                                        }
+                                    ) {
+                                        Text(
+                                            text = "+"
+                                        )
+                                    }
+                                }
+                            }
+                        )
                     }
+                    item {
+                        Button(
+                            onClick = {
+                                // TODO: open add workout plan part
+                            }
+                        ) {
+                            Icon(Icons.Filled.AddCircle, "add workout plan part")
+                            Text(
+                                text = stringResource(Res.string.addWorkoutPlanPart)
+                            )
+                        }
+                    }
+                }
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(8.dp),
+            ) {
+                Button(
+                    onClick = { workout?.let { vm.saveWorkoutPlan(workoutPlan = it, parts = planParts) } },
+                ) {
+                    Icon(Icons.Filled.Done, "save plan")
+                    Text(
+                        text = stringResource(Res.string.save)
+                    )
                 }
             }
         }
